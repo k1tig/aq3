e9b899239161f8e3ef07c44067a4241136f3a87e
Max Holden <m603holden@gmail.com>
2024-01-24 17:24:00 -0500

    clean plant list fixed

--- scape/views.py
+++ scape/views.py
@@ -14,16 +14,20 @@ class  ScapeDetailView(DetailView):
     template_name = 'scape_details.html'
 
 def profile_view(request, pk):
-
+    clean_list=[]
+    plant_list=[]
     
     profile_obj = Profile.objects.get(id=pk)
-    #scapes = Scape.objects.filter(owner=pk).prefetch_related('plants')
-    scapes = Scape.objects.filter(owner=1)
-    plant_list = []
+    scapes = Scape.objects.filter(owner=pk)
+
     for i in scapes:
-        plant = i.plants.all()
-        for x in plant:
+        scape = i.plants.all()
+        for x in scape:
             plant_list.append(x)
+    
+    [clean_list.append(x) for x in plant_list if x not in clean_list]    
+    
+    plant_list = clean_list
 
     
 
@@ -36,28 +40,6 @@ def profile_view(request, pk):
 
     
 
-    """ 
-   scape_obj = Scape.objects.filter(owner=pk)
-    profile_obj = Profile.objects.get(id=pk)
-
-
-
-    plant_names= []
-
-    for i in scape_obj:
-        plant_scape = i.plants.all()
-        for x in plant_scape:
-            plant_names.append(x.name)
-    
-    
    
-   
-    context = {
-        
-        'profile' : profile_obj,
-        'scapes' : scape_obj,
-        'plant_names' : plant_names,
-    }
-    """
     
     return render(request, 'user_details.html', context)
